814f1e6255675919d83339d4c4dc33099ae15f7f
Ayush <ayushvishwakarma767@gmail.com>
2023-10-30 09:32:45 -0500

    [AV] - Added date picker and dropdown box for description

--- src/Samples/WorkItemFormGroup/WorkItemFormGroup.tsx
+++ src/Samples/WorkItemFormGroup/WorkItemFormGroup.tsx
@@ -1,67 +1,100 @@
 import React from "react";
 import * as SDK from "azure-devops-extension-sdk";
-
-import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './WorkItemFormGroup.scss';
+
+import { setHours, setMinutes } from "date-fns";
 import { TextField, TextFieldStyle, TextFieldWidth } from "azure-devops-ui/TextField";
 import { ObservableValue } from "azure-devops-ui/Core/Observable";
 
-import './WorkItemFormGroup.scss';
+import { Observer } from "azure-devops-ui/Observer";
 import { showRootComponent } from "../../Common";
+import { CustomHeader, HeaderTitleArea } from "azure-devops-ui/Header";
+import { Icon } from "azure-devops-ui/Icon";
+import { Dropdown } from "azure-devops-ui/Dropdown";
+import { IListBoxItem } from "azure-devops-ui/ListBox";
 
 interface WorkItemFormGroupComponentState {
-  hours: string;
-  mins: string;
   dateValue: Date;
+  selectedDate: Date;
 }
 
 export class WorkItemFormGroupComponent extends React.Component<{}, WorkItemFormGroupComponentState> {
   private hours = new ObservableValue<string>("0");
   private mins = new ObservableValue<string>("0");
+  private selectedItem = new ObservableValue<string>("");
 
   constructor(props: {}) {
     super(props);
     this.state = {
-      hours: "0",
-      mins: "0",
-      dateValue: new Date()
+      dateValue: new Date(),
+      selectedDate: new Date()
     };
   }
 
   public componentDidMount() {
     SDK.init();
-}
+  }
+
+  private handleDateChange = (date: Date) => {
+    this.setState({ selectedDate: date });
+    if (date.getDate() === new Date().getDate()) {
+      this.setState({ dateValue: setHours(setMinutes(new Date(), 0), 18) });
+    } else {
+      this.setState({ dateValue: setHours(setMinutes(new Date(), 0), 0) });
+    }
+  };
 
   public render(): JSX.Element {
     return (
-      <div className="time-input">
-        <label htmlFor="hours">Hours: </label>
-        <TextField
-          autoAdjustHeight
-          maxLength={2}
-          autoFocus={true}
-          style={TextFieldStyle.inline}
-          className="input-hours"
-          inputType="number"
-          inputId="hours"
-          value={this.state.hours}
-          onChange={this.onChangeHours}
-          width={TextFieldWidth.standard}
-        />
-        <label htmlFor="mins">Mins: </label>
-        <TextField
-          autoAdjustHeight
-          maxLength={2}
-          autoFocus={true}
-          style={TextFieldStyle.inline}
-          className="input-mins"
-          inputType="number"
-          inputId="mins"
-          value={this.state.mins}
-          onChange={this.onChangeMins}
-          width={TextFieldWidth.standard}
-        />
-        <DatePicker selected={this.state.dateValue} onChange={this.handleDateChange} />
+      <div className="time-log-main">
+
+        <div>
+          <CustomHeader className="bolt-header-with-commandbar " separator>
+          <Icon ariaLabel="Clock icon" iconName="Clock" className="clock-icon"/>
+            <HeaderTitleArea>
+              <div className="flex-grow scroll-hidden">
+                <div className="title-m header-title" >
+                  Time Logs
+                </div>
+              </div>
+            </HeaderTitleArea>
+          </CustomHeader>
+        </div>
+
+        <div className="time-input">
+            <label htmlFor="hours" className="input-labels">Hours: </label>
+            <TextField className="input-hours" inputType="number" inputId="hours" value={this.hours} onChange={this.onChangeHours} width={TextFieldWidth.standard}/>
+            
+            <label htmlFor="mins">Mins: </label>
+            <TextField className="input-mins"  inputType="number"  inputId="mins" value={this.mins} onChange={this.onChangeMins} width={TextFieldWidth.standard}/>
+            
+            <label htmlFor="date-picker">Date: </label>
+            <DatePicker className="date-picker-input" id="date-picker" autoFocus={true} selected={this.state.selectedDate} showTimeSelect onChange={this.handleDateChange}
+              timeIntervals={1} dateFormat="MMMM d, yyyy h:mm aa"/>
+            
+            <label htmlFor="date-picker">Description: </label>
+            <Dropdown 
+            ariaLabel="Basic"
+            className="task-dropdown"
+            items={[
+              { id: "item1", text: "Item 1" },
+              { id: "item2", text: "Item 2" },
+              { id: "item3", text: "Item 3" }
+            ]}
+            onSelect={this.onSelect}/>
+            {/* <Observer selectedItem={this.selectedItem}>
+              {(props: { selectedItem: string }) => {
+                return (
+                  <span style={{ marginLeft: "8px", width: "150px" }}>
+                    Selected Item: {props.selectedItem}{" "}
+                  </span>
+                );
+              }}
+            </Observer> */}
+        </div>
       </div>
     );
   }
@@ -71,7 +104,7 @@ export class WorkItemFormGroupComponent extends React.Component<{}, WorkItemForm
     newValue: string
   ) => {
     if (/^\d{0,2}$/.test(newValue) && parseInt(newValue) >= 0 && newValue.length === 1 && parseInt(newValue) <= 9) {
-      this.hours.value = newValue;
+      this.hours.value = newValue
       // Handle other logic related to hours here
     }
   };
@@ -81,14 +114,13 @@ export class WorkItemFormGroupComponent extends React.Component<{}, WorkItemForm
     newValue: string
   ) => {
     if (/^\d{0,2}$/.test(newValue) && parseInt(newValue) >= 0 && parseInt(newValue) <= 59) {
-      this.mins.value = newValue;
-      // Handle other logic related to minutes here
+      this.mins.value = newValue      // Handle other logic related to minutes here
     }
   };
-  private handleDateChange = (date: Date) => {
-    this.setState({ dateValue: date });
-    // Handle other logic related to the date here
+  private onSelect = (event: React.SyntheticEvent<HTMLElement>, item: IListBoxItem<{}>) => {
+        this.selectedItem.value = item.text || "";
   };
+  
 }
 
 export default WorkItemFormGroupComponent;
